6a904bf5856e2ab00881e2014f6b9c54debd6802
1052986 <s9352816@gmail.com>
2019-10-15 23:33:47 +0800

    get numeral input

---
+++ weight_demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 23:11:43 2019
+
+@author: may
+"""
+
+from pre_train import collect_data
+import math
+
+def get_numeralData(inp, img):
+    numeral_list = []
+    for i in range(inp.shape[0]):
+       data  = collect_data(inp[i,1], inp[i,2], inp[i,3], inp[i,4], img)
+       
+       if data == 0:
+            numeral_list.pop()
+            print("pop")
+       else:
+            data = list(map(lambda x: math.log(x), data))
+            numeral_list.append(data)
+    
+    print(numeral_list)

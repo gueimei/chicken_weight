105b77406fef92dfe57419cc7b338a91c0a0a16c
gueimei <s9352816@gmail.com>
2019-10-16 12:46:59 +0000

    fix

--- weight_demo.py
+++ weight_demo.py
@@ -11,13 +11,11 @@ import math
 def get_numeralData(inp, img):
     numeral_list = []
     for i in range(inp.shape[0]):
-       data  = collect_data(inp[i,0], inp[i,1], inp[i,2], inp[i,3], img)
+       data  = collect_data(int(inp[i,0]), int(inp[i,1]), math.ceil(inp[i,2]), math.ceil(inp[i,3]), img)
        
        if data == 0:
-            numeral_list.pop()
             print("pop")
        else:
             data = list(map(lambda x: math.log(x), data))
             numeral_list.append(data)
     
-    print(numeral_list)

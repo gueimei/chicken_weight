17972f6e76e9394e553e83115e823996c5598a8b
1052986 <s9352816@gmail.com>
2019-10-17 21:27:07 +0800

    add predict function

--- weight_demo.py
+++ weight_demo.py
@@ -7,6 +7,8 @@ Created on Tue Oct 15 23:11:43 2019
 
 from pre_train import collect_data
 import math
+import torch.nn as nn
+import torch
 
 def get_numeralData(inp, img):
     numeral_list = []
@@ -18,3 +20,37 @@ def get_numeralData(inp, img):
        else:
             data = list(map(lambda x: math.log(x), data))
        numeral_list.append(data)
+     
+    predict_weight(numeral_list)
+       
+       
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.input = nn.Linear(8, 16)
+        self.hidden1 = nn.Linear(16, 8)
+        self.hidden2 = nn.Linear(8, 4)
+        self.output = nn.Linear(4, 1)
+    def forward(self, data):
+        pred = nn.functional.relu(self.input(data))
+        pred = nn.functional.relu(self.hidden1(pred))
+        pred = nn.functional.relu(self.hidden2(pred))
+        out = self.output(pred)
+        return out
+
+def predict_weight(prediction_data):
+    weight_path = "/home/chicken_weight/chicken_weight_first.pth"
+    model_net = Net()
+    model_net.load_state_dict(torch.load(weight_path)) #load checkpoint
+    if(torch.cuda.is_available()):
+        model_net.cuda()
+        
+    model_net.eval()  # Set in evaluation mode
+    
+    prediction_data = torch.Tensor(prediction_data)
+    if(torch.cuda.is_available()):
+        prediction_data = prediction_data.cuda()
+    with torch.no_grad(): 
+        prediction = model_net(prediction_data)
+    return prediction
+    
